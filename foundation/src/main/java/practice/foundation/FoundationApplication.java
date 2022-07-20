f1799a682e1146486a8214a16e21e53c0df7175f
Ronke Oyekunle <ronke.lola@gmail.com>
2022-07-19 22:15:13 -0500

    maven project

---
+++ foundation/src/main/java/practice/foundation/FoundationApplication.java
@@ -0,0 +1,13 @@
+package practice.foundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundationApplication.class, args);
+	}
+
+}
---
+++ foundation/src/test/java/practice/foundation/FoundationApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.foundation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
